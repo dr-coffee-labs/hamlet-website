8b3acee7b8931dde8abbd54943c22f322b6a5746
Matt Diebolt <mdiebolt@gmail.com>
2014-06-01 18:01:17 -0500

    minify JavaScript and CSS

--- config.rb
+++ config.rb
@@ -1,22 +1,12 @@
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+set :images_dir, "images"
 
 # Build-specific configuration
 configure :build do
   ignore "templates/*.haml"
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
 end
