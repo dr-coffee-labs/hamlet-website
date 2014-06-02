b6715130a19c0c9eaebfc1f575aa99002367e35e
Matt Diebolt <mdiebolt@gmail.com>
2014-06-02 15:34:56 -0500

    sprite sheet

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ set :images_dir, "images"
 # Build-specific configuration
 configure :build do
   ignore "templates/*.haml"
+  ignore "images/sprites/*.png"
 
   activate :minify_css
   activate :minify_javascript
